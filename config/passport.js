c058881a8f017fcd318af932b4edbbd3f23fa710
Anil Rayamajhi <anilrm07@gmail.com>
2016-10-05 12:02:26 -0700

    signup with passport

--- config/passport.js
+++ config/passport.js
@@ -15,3 +15,28 @@ passport.deserializeUser(function(id, done) {
 })
 
 //PASSPORT LOCAL STRATEGY:
+
+//LOCAL SIGNUP:
+passport.use('local-signup', new LocalStrategy({
+  //value should be same as we set in database; userSchema
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, function(req, email, password, done) {
+  //findOne() returns first match
+  User.findOne({'local.email': email}, function(err, user) {
+    //there was a problem
+    if(err) return done(err)
+    //email is taken
+    if(user) return done(null, false)
+    //neither of above happens then create user
+    var newUser = new User()
+    newUser.local.name = req.body.name
+    newUser.local.email = email
+    newUser.local.password = newUser.generateHash(password)
+    newUser.save(function(err) {
+      if(err) return console.log(err);
+      return done(null, newUser)
+    })
+  })
+}))
